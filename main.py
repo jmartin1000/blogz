c192177904d634d2f8c1d95028916de03cc50a26
Jenifer Martin <jenifercmartin1@gmail.com>
2018-10-23 10:36:32 -0500

    added two features: edit blog and delete blog

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
-    # added this field to have to develop function to "delete" blog
     hidden = db.Column(db.Boolean)
 
     def __init__(self, title, body):
@@ -24,8 +23,11 @@ class Blog(db.Model):
 
 @app.route('/blog')
 def show_blogs():
+    # need this if statement to deal with two diff get posts 
+    # the first is the case when we want the blog list to load
+    # the sedond is when we want to view a page with only one  specified blog
     if len(request.args) == 0:
-        blogs = Blog.query.all()
+        blogs = Blog.query.filter_by(hidden=False).all()
         return render_template('blog-list.html', blogs=blogs)
     else:
         blog_id = request.args.get('id')
@@ -42,7 +44,7 @@ def compose_blog():
         content = request.form.get('blog_text')
         #verify fields are not empty before commiting to db
         if (not blog_title) or (blog_title.strip() == ''):
-            flash("Blog need a title", 'error')
+            flash("Blogs need a title, silly!", 'error')
             return redirect('/newpost?blog_text='+content)
         elif (not content) or (content.strip() == ''):
             flash("What's a blog without content?", 'error')
@@ -61,6 +63,52 @@ def compose_blog():
     blog_title = '' if blog_title is None else blog_title
     content = '' if content is None else content
     return render_template('add-blog.html', title=blog_title, blog_text=content)
+
+# opens a template that allows user to edit blog
+@app.route('/edit-blog', methods=['POST'])
+def edit_blog():
+
+    blog_id = int(request.form.get('blog-id'))
+    blog = Blog.query.get(blog_id)
+
+    return render_template('edit-blog.html', blog=blog)
+
+# does the work of updating the entry in the database
+@app.route('/update-blog', methods=['POST'])
+def update_blog():
+
+    blog_id = int(request.form.get('blog-id'))
+    new_title = request.form.get('title')
+    new_body = request.form.get('blog_text')
+    blog = Blog.query.get(blog_id)
+    blog.title = new_title
+    blog.body = new_body
+    db.session.add(blog)
+    db.session.commit()
+    
+    return redirect('/blog?id='+str(blog_id))
+
+# asks the user to verify that they want to delete an entry
+@app.route('/delete-blog', methods=['POST'])
+def delete_blog():
+
+    blog_id = int(request.form.get('blog-id'))
+    blog = Blog.query.get(blog_id)
+
+    return render_template('verify-delete.html', blog=blog)
+
+# updates the entry to "hide" the blog entry
+@app.route('/complete-delete', methods=['POST'])
+def complete_delete():
+
+    blog_id = int(request.form.get('blog-id'))
+    blog = Blog.query.get(blog_id)
+    blog.hidden = True
+    db.session.add(blog)
+    db.session.commit()
+
+    return redirect("/blog")
+
 
 
 if __name__ == "__main__":
